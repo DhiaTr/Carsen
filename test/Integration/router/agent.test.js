848f22f201ec588f1d4683cda83767db95a6bade
Dhia Tr <trabelsidhia19@gmail.com>
2020-05-05 11:42:52 +0100

    refactored and cleaned most agent test code

--- test/Integration/router/agent.test.js
+++ test/Integration/router/agent.test.js
@@ -7,6 +7,7 @@ let server;
 let token;
 let base;
 let agent;
+let agentData;
 
 describe('/api/agent', () => {
 
@@ -20,6 +21,16 @@ describe('/api/agent', () => {
             adress: 'Street, New York, NY 10030',
             phone: '12345678'
         });
+        agentData = {
+            ID_Base: { _id: base._id, B_Name: base.B_Name },
+            FirstName: 'Agent1',
+            LastName: 'Smith',
+            phone: '22556688',
+            email: 'agent1mail@gmail.com',
+            salary: 1234,
+            password: '123456'
+        }
+        agent = new Agent(agentData);
         server = require('../../../index')
     });
     afterEach(async () => {
@@ -31,18 +42,24 @@ describe('/api/agent', () => {
 
     describe('GET /', () => {
 
+        const exec = async () => {
+            return await request(server).get('/api/agents').set('x-auth-token', token);
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).get('/api/agents');
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token provided', async () => {
-            const result = await request(server).get('/api/agents').set('x-auth-token', null);
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 200 if valid request', async () => {
-            const result = await request(server).get('/api/agents').set('x-auth-token', token);
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
@@ -67,7 +84,7 @@ describe('/api/agent', () => {
                 password: '234567'
             }
             ]);
-            const result = await request(server).get('/api/agents').set('x-auth-token', token);
+            const result = await exec();
             expect(result.body.length).toBe(2);
         });
 
@@ -75,13 +92,19 @@ describe('/api/agent', () => {
 
     describe('GET /:id', () => {
 
+        const exec = async () => {
+            return await request(server).get('/api/agents/' + agent._id).set('x-auth-token', token);
+        };
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).get('/api/agents/1');
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token provided', async () => {
-            const result = await request(server).get('/api/agents/1').set('x-auth-token', null);
+            token = null;
+            const result = await exec();
             expect(result.error.text).toBe('invalid Token.');
             expect(result.status).toBe(400);
         });
@@ -93,41 +116,23 @@ describe('/api/agent', () => {
         });
 
         it('should return 404 if no agent with the given was found', async () => {
-            const id = mongoose.Types.ObjectId();
-            const result = await request(server).get('/api/agents/' + id).set('x-auth-token', token);
+            agent._id = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.error.text).toBe('no agent with the given id was found.');
             expect(result.status).toBe(404);
         });
 
         it('should return 200 if valid request', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail@com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            const result = await request(server).get('/api/agents/' + agent._id).set('x-auth-token', token);
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('should return the requested agent if valid request', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            const result = await request(server).get('/api/agents/' + agent._id).set('x-auth-token', token);
+            const result = await exec();
             expect(Object.keys(result.body)).toEqual(
                 expect.arrayContaining(['ID_Base', 'FirstName', 'LastName', 'phone', 'email', 'salary', 'password']));
         });
@@ -137,107 +142,72 @@ describe('/api/agent', () => {
 
     describe('POST /', () => {
 
+        const exec = async () => {
+            return await request(server).post('/api/agents/').send(agentData).set('x-auth-token', token);
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).post('/api/agents/').send({});
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token provided', async () => {
-            const result = await request(server).post('/api/agents/').send({}).set('x-auth-token', null);
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
             expect(result.error.text).toBe('invalid Token.');
         });
 
         it('should return 403 if user not admin', async () => {
-            agent.isAdmin = false;
-            const token = agent.generateAuthToken();
-            const result = await request(server).post('/api/agents/').send({}).set('x-auth-token', token);
+            agentData.isAdmin = false;
+            token = agent.generateAuthToken();
+            const result = await exec();
             expect(result.status).toBe(403);
         });
 
         it('should return 400 if invalid data provided', async () => {
-            const result = await request(server).post('/api/agents/').send({}).set('x-auth-token', token);
+            agentData = {};
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 400 if inavlid base given', async () => {
-            const baseId = mongoose.Types.ObjectId();
-            const agent = {
-                ID_Base: baseId,
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            };
-            const result = await request(server).post('/api/agents/').send(agent).set('x-auth-token', token);
+            agentData.ID_Base = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.status).toBe(400);
             expect(result.error.text).toBe('invalid base id.');
         });
 
         it('should return 400 if user already exists', async () => {
             await base.save();
-            const agent = {
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            };
-            await new Agent(agent).save();
-            agent.ID_Base = base._id;
-            const result = await request(server).post('/api/agents/').send(agent).set('x-auth-token', token);
+            await agent.save();
+            agentData.ID_Base = base._id;
+            const result = await exec();
             expect(result.status).toBe(400);
             expect(result.error.text).toBe('agent already registred.');
         });
 
         it('should return 200 if valid request', async () => {
             await base.save();
-            const agent = {
-                ID_Base: base._id,
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            };
-            const result = await request(server).post('/api/agents/').send(agent).set('x-auth-token', token);
+            agentData.ID_Base = base._id;
+            const result = await exec();
             expect(result.status).toBe(200);
 
         });
 
-        it('should return 200 if valid request', async () => {
+        it('should save the agent if valid request', async () => {
             await base.save();
-            const agent = {
-                ID_Base: base._id,
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            };
-            await request(server).post('/api/agents/').send(agent).set('x-auth-token', token);
+            agentData.ID_Base = base._id;
+            await exec();
             const result = await Agent.findOne({ email: 'agent1mail@gmail.com' });
             expect(result).toBeTruthy();
         });
 
         it('should return the saved object if valid request', async () => {
             await base.save();
-            const agent = {
-                ID_Base: base._id,
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            };
-            const result = await request(server).post('/api/agents/').send(agent).set('x-auth-token', token);
+            agentData.ID_Base = base._id;
+            const result = await exec();
             expect(Object.keys(result.body)).toEqual(
                 expect.arrayContaining(['ID_Base', 'FirstName', 'LastName', 'phone', 'email', 'salary', 'password']));
         });
@@ -248,21 +218,27 @@ describe('/api/agent', () => {
 
     describe('PUT /:id', () => {
 
+        const exec = async () => {
+            return await request(server).put('/api/agents/' + agent._id).send(agentData).set('x-auth-token', token);
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).put('/api/agents/1').send({});
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token provided', async () => {
-            const result = await request(server).put('/api/agents/1').send({}).set('x-auth-token', null);
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
             expect(result.error.text).toBe('invalid Token.');
         });
 
         it('should return 403 if user not admin', async () => {
             agent.isAdmin = false;
-            const token = agent.generateAuthToken();
-            const result = await request(server).put('/api/agents/1').send({}).set('x-auth-token', token);
+            token = agent.generateAuthToken();
+            const result = await exec();
             expect(result.status).toBe(403);
         });
 
@@ -273,60 +249,33 @@ describe('/api/agent', () => {
         });
 
         it('should return 404 if no agent corresponds to the given id', async () => {
-            const id = mongoose.Types.ObjectId();
-            const result = await request(server).put('/api/agents/' + id).send({}).set('x-auth-token', token);
+            agent._id = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.status).toBe(404);
             expect(result.error.text).toBe('agent not found!');
         });
 
         it('should return 200 if valid request', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            const result = await request(server).put('/api/agents/' + agent._id).send(agent).set('x-auth-token', token);
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('should save modified agent if valid request', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            agent.LastName = 'new LastName';
-            await request(server).put('/api/agents/' + agent._id).send(agent).set('x-auth-token', token);
+            agentData.LastName = 'new LastName';
+            await exec();
             const result = await Agent.findById(agent._id);
-            expect(result).toHaveProperty('LastName', agent.LastName);
+            expect(result).toHaveProperty('LastName', agentData.LastName);
         });
 
         it('shoudl return the modified object if valid request', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            agent.LastName = 'new LastName';
-            const result = await request(server).put('/api/agents/' + agent._id).send(agent).set('x-auth-token', token);
+            agentData.LastName = 'new LastName';
+            const result = await exec();
             expect(Object.keys(result.body)).toEqual(
                 expect.arrayContaining(['ID_Base', 'FirstName', 'LastName', 'phone', 'email', 'salary', 'password']));
         });
@@ -335,13 +284,19 @@ describe('/api/agent', () => {
 
     describe('DELETE /:id', () => {
 
+        const exec = async () => {
+            return await request(server).delete('/api/agents/' + agent._id).send().set('x-auth-token', token);
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).delete('/api/agents/1').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token provided', async () => {
-            const result = await request(server).delete('/api/agents/1').send().set('x-auth-token', null);
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
             expect(result.error.text).toBe('invalid Token.');
         });
@@ -349,53 +304,34 @@ describe('/api/agent', () => {
         it('should return 403 if user not admin', async () => {
             agent.isAdmin = false;
             token = agent.generateAuthToken();
-            const result = await request(server).delete('/api/agents/1').send().set('x-auth-token', token);
+            const result = await exec();
             expect(result.status).toBe(403);
         });
 
         it('should return 400 if inavlid id given', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
             const result = await request(server).delete('/api/agents/1').send().set('x-auth-token', token);
             expect(result.status).toBe(400);
             expect(result.error.text).toBe('Invalid id provided.');
         });
 
         it('should return 404 if no agent corresponds to the given id', async () => {
-            const id = mongoose.Types.ObjectId();
-            const result = await request(server).delete('/api/agents/' + id).send().set('x-auth-token', token);
+            agent._id = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.status).toBe(404);
             expect(result.error.text).toBe('agent not found!');
         });
 
         it('should return 200 if valid request', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            const result = await request(server).delete('/api/agents/' + agent._id).send().set('x-auth-token', token);
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('the agent with the given id shouldn\'t exist in the db', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            await request(server).delete('/api/agents/' + agent._id).send().set('x-auth-token', token);
+            await exec();
 
             const result = await Agent.findById(agent._id);
             expect(result).not.toBeTruthy();
@@ -403,17 +339,8 @@ describe('/api/agent', () => {
 
         it('should return the deleted agent data', async () => {
             await base.save();
-            const agent = new Agent({
-                ID_Base: { _id: base._id, B_Name: base.B_Name },
-                FirstName: 'Agent1',
-                LastName: 'Smith',
-                phone: '22556688',
-                email: 'agent1mail@gmail.com',
-                salary: 1234,
-                password: '123456'
-            });
             await agent.save();
-            const result = await request(server).delete('/api/agents/' + agent._id).send().set('x-auth-token', token);
+            const result = await exec();
 
             expect(Object.keys(result.body)).toEqual(
                 expect.arrayContaining(['ID_Base', 'FirstName', 'LastName', 'phone', 'email', 'salary', 'password']));
