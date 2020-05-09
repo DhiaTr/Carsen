21797faaf7408bc8cb5abd0202f623e7cc5b7b89
Dhia Tr <trabelsidhia19@gmail.com>
2020-05-09 15:24:22 +0100

    mechanic route partially finished

--- router/mechanics.js
+++ router/mechanics.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const { Mechanic } = require('../models/mechanic');
+const mongoose = require('mongoose');
+const { Mechanic, validateMechanic } = require('../models/mechanic');
+const { Base } = require('../models/base');
 
 const auth = require('../middlewares/auth');
+const admin = require('../middlewares/admin');
 
 router.get('/', auth, async (req, res) => {
-    res.send(await Mechanic.find());
+    res.send(await Mechanic.find().populate('ID_Base'));
+});
+
+router.get('/:id', auth, async (req, res) => {
+
+    const result = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (!result) return res.status(400).send('invalid id.');
+
+    const mechanic = await Mechanic.findById(req.params.id);
+    if (!mechanic) return res.status(404).send('no mechanic with the given id was found.');
+
+    res.send(await Mechanic.findById(req.params.id).populate('ID_Base'));
+
+});
+
+router.post('/', [auth, admin], async (req, res) => {
+
+    const { error } = validateMechanic(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const base = await Base.findById(req.body.ID_Base);
+    if (!base) return res.status(400).send('Invalid Base.');
+
+
+    res.send();
 });
 
 
--- test/Integration/router/mechanic.test.js
+++ test/Integration/router/mechanic.test.js
@@ -41,24 +41,26 @@ describe('/api/mechanic', () => {
 
         it('should return the list of mechanics if valid request', async () => {
             const token = new Agent().generateAuthToken();
-            base = new Base({
+            const base = new Base({
                 B_Name: 'Base1',
                 Region: 'Region1',
                 city: 'city1',
                 adress: 'Street, New York, NY 10030',
                 phone: '12345678'
             });
-            base.save();
-            Mechanic.collection.insertMany([{
+            await base.save();
+            await Mechanic.collection.insertMany([{
+                ID_Base: base._id,
                 FirstName: 'FirstName M1',
                 LastName: 'LastName M1',
                 phone: '11223344',
-                salary: 800,
+                salary: 800
             }, {
+                ID_Base: base._id,
                 FirstName: 'FirstName M2',
                 LastName: 'LastName M2',
                 phone: '22445588',
-                salary: 777,
+                salary: 777
             }]);
             const result = await request(server).get('/api/mechanics').set('x-auth-token', token).send();
             expect(result.body.length).toBe(2);
@@ -66,23 +68,162 @@ describe('/api/mechanic', () => {
 
     });
 
-    // GET /:id
-    //  should return 401 if user not logged in 
-    //  should return 400 if invalid token provided
-    //  should return 400 if invalid id provided
-    //  should return 404 if no mechanic with the given id was found
-    //  should return 200 if valid request
-    //  should return the requested mechanic if valid request
+    describe('GET /:id', () => {
 
-    // POST /
-    //  should return 401 if user not logged in
-    //  should return 400 if invalid token provided
-    //  should return 403 if user not admin
-    //  should return 400 if invalid base provided
-    //  should return 400 if invalid data provided
-    //  should return 200 if valid request
-    //  should save the mechanic data if valid request
-    //  should return the new mechanic if valid request
+        it('should return 401 if user not logged in', async () => {
+            const result = await request(server).get('/api/mechanics/1').send();
+            expect(result.status).toBe(401);
+        });
+
+        it('should return 400 if invalid token provided', async () => {
+            const result = await request(server).get('/api/mechanics/1').set('x-auth-token', null).send();
+            expect(result.status).toBe(400);
+            expect(result.error.text).toBe('invalid Token.');
+        });
+
+        it('should return 400 if invalid id provided', async () => {
+            const token = new Agent().generateAuthToken();
+            const result = await request(server).get('/api/mechanics/1').set('x-auth-token', token).send();
+            expect(result.status).toBe(400);
+            expect(result.error.text).toBe('invalid id.');
+        });
+
+        it('should return 404 if no mechanic with the given id was found', async () => {
+            const token = new Agent().generateAuthToken();
+            const id = mongoose.Types.ObjectId();
+            const result = await request(server).get('/api/mechanics/' + id).set('x-auth-token', token).send();
+            expect(result.status).toBe(404);
+        });
+
+        it('should return 200 if valid request', async () => {
+            const token = new Agent().generateAuthToken();
+            const base = new Base({
+                B_Name: 'Base1',
+                Region: 'Region1',
+                city: 'city1',
+                adress: 'Street, New York, NY 10030',
+                phone: '12345678'
+            });
+            await base.save();
+            const mechanic = new Mechanic({
+                ID_Base: base._id,
+                FirstName: 'FirstName M2',
+                LastName: 'LastName M2',
+                phone: '22445588',
+                salary: 777
+            });
+            await mechanic.save();
+            const result = await request(server).get('/api/mechanics/' + mechanic._id).set('x-auth-token', token).send();
+            expect(result.status).toBe(200);
+        });
+
+        it('should return 200 if valid request', async () => {
+            const token = new Agent().generateAuthToken();
+            const base = new Base({
+                B_Name: 'Base1',
+                Region: 'Region1',
+                city: 'city1',
+                adress: 'Street, New York, NY 10030',
+                phone: '12345678'
+            });
+            await base.save();
+            const mechanic = new Mechanic({
+                ID_Base: base._id,
+                FirstName: 'FirstName M2',
+                LastName: 'LastName M2',
+                phone: '22445588',
+                salary: 777
+            });
+            await mechanic.save();
+            const result = await request(server).get('/api/mechanics/' + mechanic._id).set('x-auth-token', token).send();
+            expect(result.body).toHaveProperty('phone', mechanic.phone);
+            expect(result.body).toHaveProperty('FirstName', mechanic.FirstName);
+        });
+    });
+
+    describe('POST /', () => {
+
+        it('should return 401 if user not logged in', async () => {
+            const result = await request(server).post('/api/mechanics/').send();
+            expect(result.status).toBe(401);
+        });
+
+        it('should return 400 if invalid token provided', async () => {
+            const result = await request(server).post('/api/mechanics/').set('x-auth-token', null).send();
+            expect(result.status).toBe(400);
+        });
+
+        it('should return 403 if user not admin', async () => {
+            const token = new Agent().generateAuthToken();
+            const result = await request(server).post('/api/mechanics/').set('x-auth-token', token).send();
+            expect(result.status).toBe(403);
+        });
+
+        it('should return 400 if invalid data provided', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const result = await request(server).post('/api/mechanics/').set('x-auth-token', token).send({});
+            expect(result.status).toBe(400);
+        });
+
+        it('should return 400 if invalid base provided', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const baseId = mongoose.Types.ObjectId();
+            const mechanic = {
+                ID_Base: baseId,
+                FirstName: 'FirstName M2',
+                LastName: 'LastName M2',
+                phone: '22445588',
+                salary: 777
+            };
+            const result = await request(server).post('/api/mechanics/').set('x-auth-token', token).send(mechanic);
+            expect(result.status).toBe(400);
+            expect(result.error.text).toBe('Invalid Base.')
+        });
+
+        it('should return 200 if valid request', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const base = new Base({
+                B_Name: 'Base1',
+                Region: 'Region1',
+                city: 'city1',
+                adress: 'Street, New York, NY 10030',
+                phone: '12345678'
+            });
+            await base.save();
+            const mechanic = {
+                ID_Base: base._id,
+                FirstName: 'FirstName M2',
+                LastName: 'LastName M2',
+                phone: '22445588',
+                salary: 777
+            };
+            const result = await request(server).post('/api/mechanics/').set('x-auth-token', token).send(mechanic);
+            expect(result.status).toBe(200);
+        });
+
+        it('should return save the mechanic if valid request', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const base = new Base({
+                B_Name: 'Base1',
+                Region: 'Region1',
+                city: 'city1',
+                adress: 'Street, New York, NY 10030',
+                phone: '12345678'
+            });
+            await base.save();
+            const mechanic = {
+                ID_Base: base._id,
+                FirstName: 'FirstName M2',
+                LastName: 'LastName M2',
+                phone: '22445588',
+                salary: 777
+            };
+            const result = await request(server).post('/api/mechanics/').set('x-auth-token', token).send(mechanic);
+            expect(result.body).toHaveProperty('phone', mechanic.phone);
+        });
+
+        //  should return the new mechanic if valid request
+    });
 
     // PUT /:id
     //  should return 401 if user not logged in
