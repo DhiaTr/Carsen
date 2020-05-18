9983e6567fb23325238f8e8e7aac1b6afc0c8d91
Dhia Tr <trabelsidhia19@gmail.com>
2020-05-18 12:44:03 +0100

    refactored clien test code

--- test/Integration/router/client.test.js
+++ test/Integration/router/client.test.js
@@ -4,14 +4,28 @@ const request = require('supertest');
 const { Agent } = require('../../../models/agent');
 const { Client } = require('../../../models/client');
 
+let clientData;
+let client;
 let server;
-
-
+let token;
+let id;
 
 
 describe('/api/client', () => {
 
-    beforeEach(() => { server = require('../../../index'); });
+    beforeEach(() => {
+        server = require('../../../index');
+        token = new Agent({ isAdmin: true }).generateAuthToken();
+        clientData = {
+            CIN: '11111111',
+            FirstName: 'FirstNameClient1',
+            LastName: 'LastNameClient1',
+            phone: '11111111',
+            address: 'Client 1 Address Line.........'
+        };
+        client = new Client(clientData);
+        id = client._id;
+    });
     afterEach(async () => {
         await server.close();
         await Agent.remove({});
@@ -21,19 +35,24 @@ describe('/api/client', () => {
 
     describe('GET /', () => {
 
+        const exec = async () => {
+            return await request(server).get('/api/clients').set('x-auth-token', token).send();
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).get('/api/clients').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token given', async () => {
-            const result = await request(server).get('/api/clients').set('x-auth-token', null).send();
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 200 if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).get('/api/clients').set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
@@ -51,62 +70,50 @@ describe('/api/client', () => {
                 phone: '22222222',
                 address: 'Client 2 Address Line.........'
             }]);
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).get('/api/clients').set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.body.length).toBe(2);
         });
     });
 
     describe('GET /:id', () => {
 
+        const exec = async () => {
+            return await request(server).get('/api/clients/' + id).set('x-auth-token', token).send();
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).get('/api/clients/1').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token given', async () => {
-            const result = await request(server).get('/api/clients/1').set('x-auth-token', null).send();
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 400 if invalid id given', async () => {
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).get('/api/clients/1').set('x-auth-token', token).send();
+            id = '1';
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 404 if no client corresponds to the given id', async () => {
-            const token = new Agent().generateAuthToken();
-            const id = mongoose.Types.ObjectId();
-            const result = await request(server).get('/api/clients/' + id).set('x-auth-token', token).send();
+            id = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.status).toBe(404);
         });
 
         it('should return 200 if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const client = new Client({
-                CIN: '22222222',
-                FirstName: 'FirstNameClient2',
-                LastName: 'LastNameClient2',
-                phone: '22222222',
-                address: 'Client 2 Address Line.........'
-            });
             client.save();
-            const result = await request(server).get('/api/clients/' + client._id).set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('should return the requested client if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const client = new Client({
-                CIN: '22222222',
-                FirstName: 'FirstNameClient2',
-                LastName: 'LastNameClient2',
-                phone: '22222222',
-                address: 'Client 2 Address Line.........'
-            });
             client.save();
-            const result = await request(server).get('/api/clients/' + client._id).set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.body).toHaveProperty('phone', client.phone);
         });
 
@@ -114,74 +121,47 @@ describe('/api/client', () => {
 
     describe('POST /', () => {
 
+        const exec = async () => {
+            return await request(server).post('/api/clients/').set('x-auth-token', token).send();
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).post('/api/clients/').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token given', async () => {
-            const result = await request(server).post('/api/clients/').set('x-auth-token', null).send();
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 400 if invalid data given', async () => {
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).post('/api/clients/').set('x-auth-token', token).send({});
+            clientData = {};
+            const result = await exec()
             expect(result.status).toBe(400);
         });
 
         it('should return 400 if client already exists in db', async () => {
-            const token = new Agent().generateAuthToken();
-            const clientData = {
-                CIN: '22222222',
-                FirstName: 'FirstNameClient2',
-                LastName: 'LastNameClient2',
-                phone: '22222222',
-                address: 'Client 2 Address Line.........'
-            }
-            const client = new Client(clientData);
             await client.save();
-            const result = await request(server).post('/api/clients/').set('x-auth-token', token).send(clientData);
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 200 if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const clientData = {
-                CIN: '22222222',
-                FirstName: 'FirstNameClient2',
-                LastName: 'LastNameClient2',
-                phone: '22222222',
-                address: 'Client 2 Address Line.........'
-            }
-            const result = await request(server).post('/api/clients/').set('x-auth-token', token).send(clientData);
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('should save the client in the db if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const clientData = {
-                CIN: '22222222',
-                FirstName: 'FirstNameClient2',
-                LastName: 'LastNameClient2',
-                phone: '22222222',
-                address: 'Client 2 Address Line.........'
-            }
-            await request(server).post('/api/clients/').set('x-auth-token', token).send(clientData);
+            await exec();
             const result = await Client.findOne({ phone: clientData.phone });
             expect(result).toBeTruthy();
         });
 
         it('should save the client in the db if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const clientData = {
-                CIN: '22222222',
-                FirstName: 'FirstNameClient2',
-                LastName: 'LastNameClient2',
-                phone: '22222222',
-                address: 'Client 2 Address Line.........'
-            }
-            const result = await request(server).post('/api/clients/').set('x-auth-token', token).send(clientData);
+            const result = await exec();
             expect(result.body).toHaveProperty('phone', clientData.phone);
         });
 
@@ -189,171 +169,117 @@ describe('/api/client', () => {
 
     describe('PUT /:id', () => {
 
+        const exec = async () => {
+            return await request(server).put('/api/clients/' + id).set('x-auth-token', token).send();
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).put('/api/clients/1').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token given', async () => {
-            const result = await request(server).put('/api/clients/1').set('x-auth-token', null).send();
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 400 if invalid id given', async () => {
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).put('/api/clients/1').set('x-auth-token', token).send();
+            id = '1';
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 404 if no client corresponds to the given id', async () => {
-            const token = new Agent().generateAuthToken();
-            const id = mongoose.Types.ObjectId();
-            const result = await request(server).put('/api/clients/' + id).set('x-auth-token', token).send();
+            id = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.status).toBe(404);
         });
 
         it('should return 400 if invalid data given', async () => {
-            const token = new Agent().generateAuthToken();
-            const client = new Client({
-                CIN: '22222222',
-                FirstName: 'FirstNameClient2',
-                LastName: 'LastNameClient2',
-                phone: '22222222',
-                address: 'Client 2 Address Line.........'
-            });
+            clientData = {};
             await client.save();
-            const result = await request(server).put('/api/clients/' + client._id).set('x-auth-token', token).send({});
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 200 if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const clientData = {
-                CIN: '11111111',
-                FirstName: 'FirstNameClient1',
-                LastName: 'LastNameClient1',
-                phone: '11111111',
-                address: 'Client 1 Address Line.........'
-            };
-            const client = new Client(clientData);
             await client.save();
-            const result = await request(server).put('/api/clients/' + client._id).set('x-auth-token', token).send(clientData);
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('should save the changes in the db if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const clientData = {
-                CIN: '11111111',
-                FirstName: 'FirstNameClient1',
-                LastName: 'LastNameClient1',
-                phone: '11111111',
-                address: 'Client 1 Address Line.........'
-            };
-            const client = new Client(clientData);
             await client.save();
             clientData.FirstName = 'modified first name';
-            await request(server).put('/api/clients/' + client._id).set('x-auth-token', token).send(clientData);
+            await exec();
             const result = await Client.findOne({ FirstName: clientData.FirstName });
             expect(result).toBeTruthy();
         });
 
         it('should return the modified client if valid request', async () => {
-            const token = new Agent().generateAuthToken();
-            const clientData = {
-                CIN: '11111111',
-                FirstName: 'FirstNameClient1',
-                LastName: 'LastNameClient1',
-                phone: '11111111',
-                address: 'Client 1 Address Line.........'
-            };
-            const client = new Client(clientData);
             await client.save();
             clientData.FirstName = 'modified first name';
-            const result = await request(server).put('/api/clients/' + client._id).set('x-auth-token', token).send(clientData);
+            const result = await exec();
             expect(result.body).toHaveProperty('phone', clientData.phone);
         });
     });
 
     describe('DELETE /:id', () => {
 
+        const exec = async () => {
+            return await request(server).delete('/api/clients/' + id).set('x-auth-token', token).send();
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).delete('/api/clients/1').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token given', async () => {
-            const result = await request(server).delete('/api/clients/1').set('x-auth-token', null).send();
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 403 if user not admin', async () => {
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).delete('/api/clients/1').set('x-auth-token', token).send();
+            token = new Agent().generateAuthToken();
+            const result = await exec();
             expect(result.status).toBe(403);
         });
 
         it('should return 400 if invalid id given', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const result = await request(server).delete('/api/clients/1').set('x-auth-token', token).send();
+            id = '1';
+            const result = await exec();
             console.log(result.error.text);
             expect(result.status).toBe(400);
         });
 
         it('should return 404 if no client corresponds to the given id', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const id = mongoose.Types.ObjectId();
-            const result = await request(server).delete('/api/clients/' + id).set('x-auth-token', token).send();
+            id = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.status).toBe(404);
         });
 
         it('should return 200 if valid request', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const clientData = {
-                CIN: '11111111',
-                FirstName: 'FirstNameClient1',
-                LastName: 'LastNameClient1',
-                phone: '11111111',
-                address: 'Client 1 Address Line.........'
-            };
-            const client = new Client(clientData);
             await client.save();
-            const result = await request(server).delete('/api/clients/' + client._id).set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('the client with the given id shouldn\'t exist in the db if valid request', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const clientData = {
-                CIN: '11111111',
-                FirstName: 'FirstNameClient1',
-                LastName: 'LastNameClient1',
-                phone: '11111111',
-                address: 'Client 1 Address Line.........'
-            };
-            const client = new Client(clientData);
             await client.save();
-            await request(server).delete('/api/clients/' + client._id).set('x-auth-token', token).send();
+            await exec();
             const result = await Client.findOne({ phone: clientData.phone });
             expect(result).not.toBeTruthy();
         });
 
         it('should return the deleted client if valid request', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const clientData = {
-                CIN: '11111111',
-                FirstName: 'FirstNameClient1',
-                LastName: 'LastNameClient1',
-                phone: '11111111',
-                address: 'Client 1 Address Line.........'
-            };
-            const client = new Client(clientData);
             await client.save();
-            const result = await request(server).delete('/api/clients/' + client._id).set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.body).toHaveProperty('phone', clientData.phone);
         });
-
-
     });
-
 });
