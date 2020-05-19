a01aea247c31debba6e24ac8c7b77b4c967f9cbf
Dhia Tr <trabelsidhia19@gmail.com>
2020-05-19 13:12:58 +0100

    refactored archived order test code

--- test/Integration/router/archived_order.test.js
+++ test/Integration/router/archived_order.test.js
@@ -4,11 +4,27 @@ const request = require('supertest');
 const { Agent } = require('../../../models/agent');
 const { ArchivedOrder } = require('../../../models/archived_order');
 
+let archived_order;
+let ID_mock;
 let server;
+let token;
+let id;
 
 describe('/api/archived_orders', () => {
 
-    beforeEach(() => { server = require('../../../index'); })
+    beforeEach(() => {
+        server = require('../../../index');
+        token = new Agent({ isAdmin: true }).generateAuthToken();
+        ID_mock = mongoose.Types.ObjectId();
+        archived_order = new ArchivedOrder({
+            ID_Client: ID_mock,
+            ID_Car: ID_mock,
+            Order_Date: '2002-12-10T00:00:00.000Z',
+            Rent_Start_Date: '2002-12-22T00:00:00.000Z',
+            Rent_End_Date: '2003-01-22T00:00:00.000Z'
+        });
+        id = archived_order._id;
+    });
     afterEach(async () => {
         await server.close();
         await ArchivedOrder.remove({});
@@ -17,31 +33,34 @@ describe('/api/archived_orders', () => {
 
     describe('GET /', () => {
 
+        const exec = async () => {
+            return await request(server).get('/api/archived_orders').set('x-auth-token', token).send();
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).get('/api/archived_orders').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token given', async () => {
-            const result = await request(server).get('/api/archived_orders').set('x-auth-token', null).send();
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 403 user not admin', async () => {
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).get('/api/archived_orders').set('x-auth-token', token).send();
+            token = new Agent().generateAuthToken();
+            const result = await exec();
             expect(result.status).toBe(403);
         });
 
         it('should return 200 if valid request', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const result = await request(server).get('/api/archived_orders').set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('should return the list or archived orders if valid request', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const ID_mock = mongoose.Types.ObjectId();
             await ArchivedOrder.collection.insertMany([{
                 ID_Client: ID_mock,
                 ID_Car: ID_mock,
@@ -55,69 +74,56 @@ describe('/api/archived_orders', () => {
                 Rent_Start_Date: '2002-12-22T00:00:00.000Z',
                 Rent_End_Date: '2003-01-22T00:00:00.000Z'
             }])
-            const result = await request(server).get('/api/archived_orders').set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.body.length).toBe(2);
         });
     });
 
     describe('GET /:id', () => {
 
+        const exec = async () => {
+            return await request(server).get('/api/archived_orders/' + id).set('x-auth-token', token).send();
+        }
+
         it('should return 401 if user not logged in', async () => {
-            const result = await request(server).get('/api/archived_orders/1').send();
+            token = '';
+            const result = await exec();
             expect(result.status).toBe(401);
         });
 
         it('should return 400 if invalid token given', async () => {
-            const result = await request(server).get('/api/archived_orders/1').set('x-auth-token', null).send();
+            token = null;
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 403 user not admin', async () => {
-            const token = new Agent().generateAuthToken();
-            const result = await request(server).get('/api/archived_orders/1').set('x-auth-token', token).send();
+            token = new Agent().generateAuthToken();
+            const result = await exec();
             expect(result.status).toBe(403);
         });
 
         it('should return 400 if invalid id given', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const result = await request(server).get('/api/archived_orders/1').set('x-auth-token', token).send();
+            id = '1';
+            const result = await exec();
             expect(result.status).toBe(400);
         });
 
         it('should return 404 if not archived order corresponds to the given id', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const id = mongoose.Types.ObjectId();
-            const result = await request(server).get('/api/archived_orders/' + id).set('x-auth-token', token).send();
+            id = mongoose.Types.ObjectId();
+            const result = await exec();
             expect(result.status).toBe(404);
         });
 
         it('should return 200 if valid request', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const ID_mock = mongoose.Types.ObjectId();
-            const archived_order = new ArchivedOrder({
-                ID_Client: ID_mock,
-                ID_Car: ID_mock,
-                Order_Date: '2002-12-10T00:00:00.000Z',
-                Rent_Start_Date: '2002-12-22T00:00:00.000Z',
-                Rent_End_Date: '2003-01-22T00:00:00.000Z'
-            });
             await archived_order.save();
-            const result = await request(server).get('/api/archived_orders/' + archived_order._id).set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.status).toBe(200);
         });
 
         it('should return the requested archived_order if valid request', async () => {
-            const token = new Agent({ isAdmin: true }).generateAuthToken();
-            const ID_mock = mongoose.Types.ObjectId();
-            const archived_order = new ArchivedOrder({
-                ID_Client: ID_mock,
-                ID_Car: ID_mock,
-                Order_Date: '2002-12-10T00:00:00.000Z',
-                Rent_Start_Date: '2002-12-22T00:00:00.000Z',
-                Rent_End_Date: '2003-01-22T00:00:00.000Z'
-            });
             await archived_order.save();
-            const result = await request(server).get('/api/archived_orders/' + archived_order._id).set('x-auth-token', token).send();
+            const result = await exec();
             expect(result.body).toHaveProperty('Order_Date', '2002-12-10T00:00:00.000Z');
             expect(result.body).toHaveProperty('Rent_Start_Date', '2002-12-22T00:00:00.000Z');
             expect(result.body).toHaveProperty('Rent_End_Date', '2003-01-22T00:00:00.000Z');
