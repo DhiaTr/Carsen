7d17e6d454e72556a3223ab30644d78f51d9c190
Dhia Tr <trabelsidhia19@gmail.com>
2020-05-20 20:30:57 +0100

    order route

--- router/order.js
+++ router/order.js
@@ -7,6 +7,7 @@ const { Client } = require('../models/client');
 const { Car } = require('../models/car');
 
 const auth = require('../middlewares/auth');
+const admin = require('../middlewares/admin');
 
 router.get('/', auth, async (req, res) => {
     res.send(await Order.find());
@@ -56,8 +57,42 @@ router.put('/:id', auth, async (req, res) => {
     const idStatus = mongoose.Types.ObjectId.isValid(req.params.id);
     if (!idStatus) return res.status(400).send('invalid id.');
 
-    const order = await Order.findById(req.params.id);
+    let order = await Order.findById(req.params.id);
     if (!order) return res.status(404).send('no order with the given id was found.');
+
+    const { error } = validateOrder(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const client = await Client.findById(req.body.ID_Client);
+    if (!client) return res.status(400).send('no client with the given id was found.');
+
+    const car = await Car.findById(req.body.ID_Car);
+    if (!car) return res.status(400).send('no car with the given id was found');
+
+    order = new Order({
+        ID_Client: req.body.ID_Client,
+        ID_Car: req.body.ID_Car,
+        Order_Date: req.body.Order_Date,
+        Rent_Start_Date: req.body.Rent_Start_Date,
+        Rent_End_Date: req.body.Rent_End_Date
+    });
+    await order.save();
+    res.send(order);
 });
+
+router.delete('/:id', [auth, admin], async (req, res) => {
+
+    const idStatus = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (!idStatus) return res.status(400).send('invalid id.');
+
+    let order = await Order.findById(req.params.id);
+    if (!order) return res.status(404).send('no order with the given id was found.');
+
+    order = await Order.findByIdAndDelete(req.params.id);
+
+    res.send(order);
+
+});
+
 
 module.exports = router;
--- test/Integration/router/order.test.js
+++ test/Integration/router/order.test.js
@@ -321,36 +321,238 @@ describe('/api/orders', () => {
             expect(result.status).toBe(404);
         });
 
-        //  should return 404 if order dosent exist
-        //  should return 400 if invalid data given
-        //  should return 400 if no client with the given id exists
-        //  should return 400 if no car with the given id exists
-        //  should return 200 if valid request
-        //  should save the changes in the db if valid request
-        //  should return the changed order if valid request
+        it('should return 400 if invalid data given', async () => {
+            const token = new Agent().generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const order = new Order({
+                ID_Client: ID_mock,
+                ID_Car: ID_mock,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            });
+            await order.save();
+            const result = await request(server).put('/api/orders/' + order._id).set('x-auth-token', token).send({});
+            expect(result.status).toBe(400);
+        });
+
+        it('should return 400 if no client with the given id exists', async () => {
+            const token = new Agent().generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const orderData = {
+                ID_Client: ID_mock,
+                ID_Car: ID_mock,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            };
+            const order = new Order(orderData);
+            await order.save();
+            const result = await request(server).put('/api/orders/' + order._id).set('x-auth-token', token).send(orderData);
+            expect(result.status).toBe(400);
+        });
+
+        it('should return 400 if no car with the given id exists', async () => {
+            const token = new Agent().generateAuthToken();
+            const client = new Client({
+                CIN: '11111111',
+                FirstName: 'FirstNameClient1',
+                LastName: 'LastNameClient1',
+                phone: '11111111',
+                address: 'Client 1 Address Line.........'
+            });
+            client.save();
+            const ID_mock = mongoose.Types.ObjectId();
+            const orderData = {
+                ID_Client: client._id,
+                ID_Car: ID_mock,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            };
+            const order = new Order(orderData);
+            await order.save();
+            const result = await request(server).put('/api/orders/' + order._id).set('x-auth-token', token).send(orderData);
+            expect(result.status).toBe(400);
+        });
+
+        it('should return 200 if valid request', async () => {
+            const token = new Agent().generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const client = new Client({
+                CIN: '11111111',
+                FirstName: 'FirstNameClient1',
+                LastName: 'LastNameClient1',
+                phone: '11111111',
+                address: 'Client 1 Address Line.........'
+            });
+            await client.save();
+            const car = new Car({
+                ID_Base: ID_mock,
+                Mark: 'Mark1',
+                Model: 'Model1',
+                Registration_Number: '123',
+                production_Year: '1234',
+                Rent_Price: 50,
+                Category: 'category1'
+            });
+            await car.save();
+            const orderData = {
+                ID_Client: client._id,
+                ID_Car: car._id,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            };
+            const order = new Order(orderData);
+            await order.save();
+            const result = await request(server).put('/api/orders/' + order._id).set('x-auth-token', token).send(orderData);
+            expect(result.status).toBe(200);
+        });
+
+        it('should save the changes if valid request', async () => {
+            const token = new Agent().generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const client = new Client({
+                CIN: '11111111',
+                FirstName: 'FirstNameClient1',
+                LastName: 'LastNameClient1',
+                phone: '11111111',
+                address: 'Client 1 Address Line.........'
+            });
+            await client.save();
+            const car = new Car({
+                ID_Base: ID_mock,
+                Mark: 'Mark1',
+                Model: 'Model1',
+                Registration_Number: '123',
+                production_Year: '1234',
+                Rent_Price: 50,
+                Category: 'category1'
+            });
+            await car.save();
+            const orderData = {
+                ID_Client: client._id,
+                ID_Car: car._id,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            };
+            const order = new Order(orderData);
+            await order.save();
+            orderData.Rent_Start_Date = '2003-04-10T00:00:00.000Z';
+            await request(server).put('/api/orders/' + order._id).set('x-auth-token', token).send(orderData);
+            const result = await Order.findOne({ ID_Client: client._id, ID_Car: car._id, Rent_Start_Date: '2003-04-10T00:00:00.000Z' });
+            expect(result).toBeTruthy();
+        });
+
+        it('should return the changed order if valid request', async () => {
+            const token = new Agent().generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const client = new Client({
+                CIN: '11111111',
+                FirstName: 'FirstNameClient1',
+                LastName: 'LastNameClient1',
+                phone: '11111111',
+                address: 'Client 1 Address Line.........'
+            });
+            await client.save();
+            const car = new Car({
+                ID_Base: ID_mock,
+                Mark: 'Mark1',
+                Model: 'Model1',
+                Registration_Number: '123',
+                production_Year: '1234',
+                Rent_Price: 50,
+                Category: 'category1'
+            });
+            await car.save();
+            const orderData = {
+                ID_Client: client._id,
+                ID_Car: car._id,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            };
+            const order = new Order(orderData);
+            await order.save();
+            orderData.Rent_Start_Date = '2003-04-10T00:00:00.000Z';
+            const result = await request(server).put('/api/orders/' + order._id).set('x-auth-token', token).send(orderData);
+            expect(Object.keys(result.body)).toEqual(
+                expect.arrayContaining(['ID_Client', 'ID_Car', 'Rent_Start_Date', 'Rent_End_Date']));
+        });
+
     });
 
-    // DELETE /:id
-    //  should return 401 if user not logged in
-    //  should return 400 if invalid token given
-    //  should return 403 if user not admin
-    //  should return 400 if invalid id given
-    //  should return 404 if order dosent exist
-    //  should return 200 if valid request
-    //  should remove the order with the givn id from the db
-    //  should return the deleted order if valid request
+    describe('DELETE /:id', () => {
+
+        it('should return 401 if user not logged in', async () => {
+            const result = await request(server).delete('/api/orders/1').send();
+            expect(result.status).toBe(401);
+        });
+
+        it('should return 400 if invalid token given', async () => {
+            const result = await request(server).delete('/api/orders/1').set('x-auth-token', null).send();
+            expect(result.status).toBe(400);
+        });
+
+        it('should return 403 if user not admin', async () => {
+            const token = new Agent().generateAuthToken();
+            const result = await request(server).delete('/api/orders/1').set('x-auth-token', token).send();
+            expect(result.status).toBe(403);
+        });
+
+        it('should return 400 if invalid id given', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const result = await request(server).delete('/api/orders/1').set('x-auth-token', token).send();
+            expect(result.status).toBe(400);
+        });
+
+        it('should return 404 if no order with the given id exists', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const id = mongoose.Types.ObjectId();
+            const result = await request(server).delete('/api/orders/' + id).set('x-auth-token', token).send();
+            expect(result.status).toBe(404);
+        });
+
+        it('should return 200 valid request', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const order = new Order({
+                ID_Client: ID_mock,
+                ID_Car: ID_mock,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            });
+            await order.save();
+            const result = await request(server).delete('/api/orders/' + order._id).set('x-auth-token', token).send();
+            expect(result.status).toBe(200);
+        });
+
+        it('should delete the order valid request', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const order = new Order({
+                ID_Client: ID_mock,
+                ID_Car: ID_mock,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            });
+            await order.save();
+            await request(server).delete('/api/orders/' + order._id).set('x-auth-token', token).send();
+            const result = await Order.findById(order._id);
+            expect(result).not.toBeTruthy();
+        });
+
+        it('should return the deleted order if valid request', async () => {
+            const token = new Agent({ isAdmin: true }).generateAuthToken();
+            const ID_mock = mongoose.Types.ObjectId();
+            const order = new Order({
+                ID_Client: ID_mock,
+                ID_Car: ID_mock,
+                Rent_Start_Date: '2002-12-10T00:00:00.000Z',
+                Rent_End_Date: '2003-01-10T00:00:00.000Z'
+            });
+            await order.save();
+            const result = await request(server).delete('/api/orders/' + order._id).set('x-auth-token', token).send();
+            expect(Object.keys(result.body)).toEqual(
+                expect.arrayContaining(['ID_Client', 'ID_Car', 'Rent_Start_Date', 'Rent_End_Date']));
+        });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 });
